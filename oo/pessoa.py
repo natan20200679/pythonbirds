7cb2dbbc07901d9fd5f71cfc4dd70ef35ac361bb
natan20200679 <natanaelmedrado@outlook.com>
2020-09-15 12:15:55 -0300

    Criados atributos de instância "nome" e "idade"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,17 @@
 class Pessoa:
+
+    def __init__(self):
+        self.nome = None
+        self.idade = None
+
     def cumprimento(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimento(p))
+    p.nome = 'Natanael'
+    p.idade = 38
+    print(p.nome)
+    print(p.idade)
+
